179349b7c8cf1d4cbd7ab8d116f6ae01cd52a47a
Farney <farney9@gmail.com>
2022-09-01 13:15:43 -0500

    rebuild

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  client_id: `a2c0b71bfd044986900f1a8b517fb26a`,
-  redirect_uri: `https://my-music-app-omega.vercel.app/login`,
+  client_id: `ee7ccacd46bd42018b1b20a8c190d32e`,
+  redirect_uri: `https://my-music-app-far.netlify.app/login`,
 };
