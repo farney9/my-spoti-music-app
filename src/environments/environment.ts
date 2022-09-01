51a3742c2c46087f8c126df5f802190c5f5c0023
Farney <farney9@gmail.com>
2022-09-01 14:23:21 -0500

    rebuild

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // client_id: `a2c0b71bfd044986900f1a8b517fb26a`,
   client_id: `ee7ccacd46bd42018b1b20a8c190d32e`,
   redirect_uri: `http://localhost:4200/login`,
+  // client_id: `a2c0b71bfd044986900f1a8b517fb26a`,
 };
